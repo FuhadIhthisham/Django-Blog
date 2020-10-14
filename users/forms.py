750740ed02ef7ea9e1efc0610767d65c2cb5e13b
Fuhad Ihthisham <ihthisham.v@gmail.com>
2020-10-15 00:08:09 +0530

    Removed hints on user registration form

--- users/forms.py
+++ users/forms.py
@@ -10,6 +10,11 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
 
 
 class UserUpdateForm(forms.ModelForm):
